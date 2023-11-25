3508ddf5e5b0f2bd8cc8e1106d00ec42c8ee3b26
neemias@thidi.com.br <neemias@thidi.com.br>
2023-11-25 08:10:59 -0300

    google search console

--- src/app/[webhookId]/layout.tsx
+++ src/app/[webhookId]/layout.tsx
@@ -3,9 +3,6 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Webhook",
   description: "Webhook Data",
-  other: {
-    "google-site-verification": "3IERGxmTxp1kF_nMZk7QVhqRxTU64mqX2bT55hFKKSk",
-  },
 };
 
 export default function RootLayout({
